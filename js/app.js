3a8855a195a97154ac563e891196cd247a6d12c5
bayanaldaoud <bayan.and95@gmail.com>
2021-02-09 05:35:17 -0800

    lab02-AboutMe

---
+++ js/app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+alert('Answer with yes[y] or no[n] please');
+let urName = prompt ('what\'s your your name??');
+alert('Hello' + ' ' + urName + ' '+ 'lets start the game ! ' );
+
+
+let myName = prompt ('what\'s my name ?? is it bayan??');
+if (myName .toLowerCase() ==='yes' || myName .toLowerCase() === 'y' ) {
+    //console.log ('correct');
+    alert ('correct');
+
+} else {
+    alert ('false');
+}
+
+let study = prompt ('what was my major study in college ??is it engineering??');
+if (study .toLowerCase() ==='no'|| study .toLowerCase === 'n'){
+//console.log ('correct');
+alert ('correct');
+
+} else {
+    alert ('false');
+}
+
+let age = prompt ('what\'s my age ?? is it 25 ??');
+if (age .toLowerCase() === 'yes' || age .toLowerCase() ==='y'){
+//console.log ('correct');
+alert ('correct');
+}else {
+    alert ('false');
+}
+let hobby = prompt ('what\'s my favorite hobby ?? is it reading??');
+if(hobby .toLowerCase() === 'yes' || hobby .toLowerCase() === 'y'){
+    //console.log ('correct');
+ alert ('correct');
+}else {
+    alert ('false');
+}
+
+let food = prompt ('what\'s my favorite food ?? is it Mansaf ??');
+if (food .toLowerCase()=== 'yes' || food .toLowerCase() === 'y'){
+ //console.log ('correct');
+alert ('correct');
+}else{
+    alert ('false');
+}
+
+alert (' nice try ' + ' ' + urName )
+
+
+
+
